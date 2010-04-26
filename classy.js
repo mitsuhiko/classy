ff1d5ec79562784967a3512a3ec134e1825aa62e
Armin Ronacher <armin.ronacher@active-4.com>
2010-04-26 21:59:32 +0200

    Initial version.

---
+++ classy.js
@@ -0,0 +1,116 @@
+/**
+ * Classy - classy classes for JavaScript
+ *
+ * :copyright: (c) 2010 by Armin Ronacher.
+ * :license: BSD.
+ */
+
+;(function(undefined) {
+  var
+    CLASSY_VERSION = '1.0',
+    root = this,
+    old_class = Class,
+    disable_constructor = false;
+
+  /* we check if $super is in use by a class if we can.  But first we have to
+     check if the JavaScript interpreter supports that.  This also matches
+     to false positives later, but that does not do any harm besides slightly
+     slowing calls down. */
+  var probe_super = (function(){$super();}).toString().indexOf('$super') > 0;
+  function usesSuper(obj) {
+    return !probe_super || /\B\$super\b/.test(obj.toString());
+  }
+
+  /* helper function to set the attribute of something to a value or
+     removes it if the value is undefined. */
+  function setOrUnset(obj, key, value) {
+    if (value === undefined)
+      delete obj[key];
+    else
+      obj[key] = value;
+  }
+
+  /* gets the own property of an object */
+  function getOwnProperty(obj, name) {
+    return Object.prototype.hasOwnProperty.call(obj, name)
+      ? obj[name] : undefined;
+  }
+
+  /* the base class we export */
+  var Class = function() {};
+
+  /* restore the global Class name and pass it to a function.  This allows
+     different versions of the classy library to be used side by side and
+     in combination with other libraries. */
+  Class.$noConflict = function() {
+    setOrUnset(root, 'Class', old_class);
+    return Class;
+  };
+
+  /* what version of classy are we using? */
+  Class.$classyVersion = CLASSY_VERSION;
+
+  /* extend functionality */
+  Class.$extend = function(properties) {
+    var super_prototype = this.prototype;
+
+    /* disable constructors and instanciate prototype.  Because the
+       prototype can't raise an exception when created, we are save
+       without a try/finally here. */
+    disable_constructor = true;
+    var prototype = new this;
+    disable_constructor = false;
+
+    /* copy all properties of the includes over if there are any */
+    if (properties.__include__)
+      for (var i = 0, n = properties.__include__.length; i != n; ++i) {
+        var mixin = properties.__include__[i];
+        for (var name in mixin) {
+          var value = getOwnProperty(mixin, name);
+          if (value !== undefined)
+            prototype[name] = mixin[name];
+        }
+      }
+
+    /* copy all properties over to the new prototype */
+    for (var name in properties) {
+      var value = getOwnProperty(properties, name);
+      if (name === '__include__' || value === undefined)
+        continue;
+
+      prototype[name] = typeof value === 'function' && usesSuper(value) ?
+        (function(meth, name) {
+          return function() {
+            var old_super = getOwnProperty(this, '$super');
+            this.$super = super_prototype[name];
+            try {
+              return meth.apply(this, arguments);
+            }
+            finally {
+              setOrUnset(this, '$super', old_super);
+            }
+          };
+        })(value, name) : value
+    }
+
+    /* dummy constructor */
+    var rv = function Class() {
+      if (disable_constructor)
+        return;
+      var proper_this = root === this ? new arguments.callee : this;
+      if (proper_this.__init__)
+        proper_this.__init__.apply(proper_this, arguments);
+      return proper_this;
+    }
+
+    /* copy prototype and constructor over, reattach $extend and
+       return the class */
+    rv.prototype = prototype;
+    rv.constructor = rv;
+    rv.$extend = Class.$extend;
+    return rv;
+  };
+
+  /* export the class */
+  root.Class = Class;
+})();
---
+++ docs/index.rst
@@ -0,0 +1,115 @@
+Classy Classes for JavaScript
+=============================
+
+Wooya.  You love JavaScript and use jQuery or another lightweight library
+extensively but you notice that your code becomes messier and messier the
+larger the application grows?  Well, that's where Classy comes in.  Classy
+is a small JavaScript library that implements Python inspired classes for
+JavaScript.
+
+Where can you get that classy library?  You can get it from `github
+<http://github.com/mitsuhiko/classy>`_.
+
+How does this look like?
+
+Defining Classes
+----------------
+
+.. sourcecode:: javascript
+
+    var Animal = Class.$extend({
+      __init__ : function(name, age) {
+        this.name = name;
+        this.age = age;
+        this.health = 100;
+      },
+
+      die : function() {
+        this.health = 0;
+      },
+
+      eat : function(what) {
+        this.health += 5;
+      }
+    });
+
+Creating Subclasses
+-------------------
+
+.. sourcecode:: javascript
+
+    var Tiger = Animal.$extend({
+      eat : function(animal) {
+        this.$super(animal);
+        animal.die();
+      }
+    });
+
+    var Sheep = Animal.$extend({
+      __init__ : function(name, age) {
+        this.$super(name, age);
+        this.shorn = false;
+      }
+    });
+
+Using These Classes
+-------------------
+
+.. sourcecode:: javascript
+
+    var leo = Tiger("Leo", 3);
+    var molli = Sheep("Molli", 1);
+    leo.eat(molli);
+
+Alternatively you can also use the `new` operator like with regular
+JavaScript classes.  But no worries, both do exactly the same:
+
+.. sourcecode:: javascript
+
+    var leo = new Tiger("Leo", 3);
+    var molli = new Sheep("Molli", 1);
+    leo.eat(molli);
+
+You want to check if an object is an instance of something?  That's
+easy as well:
+
+.. sourcecode:: javascript
+
+    var leo = new Tiger("Leo", 3);
+    leo instanceof Animal // -> true
+
+Recap and Outro
+---------------
+
+What you can do with Classy:
+
+.. js:attribute:: Class.$classyVersion
+
+   The version of Classy as string (``1.0`` for instance).
+
+.. js:function:: Class.$extends(properties)
+
+   Creates a new class by extending the given class.  The `properties`
+   is an object with class attributes and methods (functions) for this
+   class.
+
+.. js:function:: Class.$noConflict()
+
+   Removes the `Class` object from the `window` object and restores what
+   was there before Classy was loaded.  It then returns the class
+   object from the function.  This makes it possible to use multiple
+   versions of Classy side by side or in combination with other libraries
+   that also define an object with that name.  Example usage:
+
+   .. sourcecode:: javascript
+
+      (function(Class) {
+        // here you can use the Classy Class object, outside it
+        // won't be available
+      })(Class.$noConflict());
+
+.. js:function:: this.$super()
+
+   When called from within a Classy function this invokes the parent
+   function of the same name.  The arguments are forwarded directly.
+   Check out the examples above for how that works.
